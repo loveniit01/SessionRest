3e97cba3980d913e50b78dc129d28b800f06ed27
loveniit01 <loveniit01@gmail.com>
2020-02-14 21:25:14 +0530

    first commit

---
+++ src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	@RequestMapping(value = "/", produces = "application/json")
+	public Map<String, String> helloUser(@RequestParam String x) {
+		HashMap<String, String> result = new HashMap<>();
+		result.put("username", x);
+		return result;
+	}
+
+	@RequestMapping("/logout")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void logout(HttpSession session) {
+		
+		session.invalidate();
+	}
+
+}
---
+++ src/main/java/com/example/demo/SessionRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionRestApplication.class, args);
+	}
+
+}
