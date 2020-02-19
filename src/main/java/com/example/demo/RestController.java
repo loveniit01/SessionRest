fbd6c0dcc5a219faea0c506b08cfa4981b1e7cb5
Pradeep joshi <pradeep.joshi@shephertz.co.in>
2020-02-19 15:57:43 +0530

    mysql connection

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -1,14 +1,17 @@
 package com.example.demo;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.example.demo.model.Login;
+import com.example.demo.repo.Connect2Table;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -17,14 +20,21 @@ public class RestController {
 	public Map<String, String> helloUser(@RequestParam String x) {
 		HashMap<String, String> result = new HashMap<>();
 		result.put("username", x);
+		
+		List<Login> ll = ct.connect();
+		System.out.println("~~~~controller = = = "+ll);
+		
+		result.put("name", "~~~~");
 		return result;
 	}
 
 	@RequestMapping("/logout")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void logout(HttpSession session) {
-		
-		session.invalidate();
+	public void logout() {
+		System.out.println("== ho ja na logout");
+//		session.invalidate();
 	}
 
+	@Autowired
+	Connect2Table ct;
 }
